01618109b574ef14be1e4c2cab007657da0dbf59
muhammetozdamar <m.ozdmar@gmail.com>
2022-12-08 17:32:07 +0300

    chore: subscribing current damageable

--- Assets/Scripts/Gameplay/Building/Barracks.cs
+++ Assets/Scripts/Gameplay/Building/Barracks.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using BaridaGames.PanteonCaseProject.Data;
 using UnityEngine;
--- Assets/Scripts/Gameplay/UI/InformationPanelController.cs
+++ Assets/Scripts/Gameplay/UI/InformationPanelController.cs
@@ -14,10 +14,17 @@ namespace BaridaGames.PanteonCaseProject.Gameplay
         [SerializeField] private TextMeshProUGUI healthText = default;
         [SerializeField] private Image healthImage = default;
         [SerializeField] private GameObject productionsRoot = default;
-
+        private IDamageable currentDamageable;
         private void Awake()
         {
             Instance = this;
+        }
+
+        private void UpdateInformation(object sender, EventArgs e)
+        {
+            var args = (DamageableEventArgs)e;
+            healthText.text = $"{args.currentHealth}/{args.maxHealth}";
+            healthImage.fillAmount = (args.currentHealth / args.maxHealth);
         }
 
         public void SetCurrentUnit(UnitBase unit)
@@ -34,11 +41,22 @@ namespace BaridaGames.PanteonCaseProject.Gameplay
             {
                 productionsRoot.SetActive(true);
             }
+
+            if (currentDamageable != null)
+            {
+                currentDamageable.OnDamaged -= UpdateInformation;
+            }
+            currentDamageable = (IDamageable)unit;
+            currentDamageable.OnDamaged += UpdateInformation;
         }
 
         internal void ResetPanel()
         {
             panel.SetActive(false);
+            if (currentDamageable != null)
+            {
+                currentDamageable.OnDamaged -= UpdateInformation;
+            }
         }
     }
 }
